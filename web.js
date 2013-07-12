779053152c2be2f1202dc2f0e79c1c8e5179e1d2
Ubuntu <ubuntu@ip-172-31-44-188.us-west-2.compute.internal>
2013-07-12 02:07:42 +0000

    Modified web.js to point to index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+far fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
