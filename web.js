23abb4c74be0e093dd1a3e6a499d8e088f685db0
Ubuntu <ubuntu@ip-172-31-44-188.us-west-2.compute.internal>
2013-07-12 01:35:07 +0000

    Fiddling with web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.writeFileSync('index.html');
+  fs.writeFileSync('index.html', 'Hello World from index.html', 'utf8');
 });
 
 var port = process.env.PORT || 5000;
